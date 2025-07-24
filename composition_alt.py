e7a4d1bf7003db1c5f9f6f54df48c127ae340316
IsaacRWI <isaacngrwi@gmail.com>
2025-07-24 19:57:31 +0800

    Updated position variable for the clips to use preset positions

--- composition_alt.py
+++ composition_alt.py
@@ -1,11 +1,10 @@
 from moviepy import *
 
 class Clip:
-    def __init__(self, cl, dur, posx, posy):
+    def __init__(self, cl, dur, pos):
         self.cl = cl
         self.dur = int(dur)
-        self.posx = posx
-        self.posy = posy
+        self.pos = pos
 
     def opening_layout(self):
         clip = VideoFileClip(self.cl).subclipped(0, self.dur).with_position((self.posx, self.posy)).resized(width=500)
