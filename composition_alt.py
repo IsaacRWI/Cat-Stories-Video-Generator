26daa9e04f2ef43c17cb8c17f9aa4ea26e62cd87
IsaacNgRWI <isaacngrwi@gmail.com>
2025-07-26 15:19:59 +0800

    Changed position presets to use a dictionary instead of switch cases

--- composition_alt.py
+++ composition_alt.py
@@ -4,12 +4,15 @@ class Clip:
     def __init__(self, cl, dur, pos):
         self.cl = cl
         self.dur = int(dur)
-        match pos:
+        """match pos:
             case center:
                 self.pos = (540, 1280)
+        """
+        positions = {"center" : (540, 1280), "center_l" : (360, 1280), "center_r" : (720, 1280)}
+        self.pos = positions[pos]
 
     def opening_layout(self):
-        center = (540, 1280)
+        # center = (540, 1280)
         # clip = VideoFileClip(self.cl).subclipped(0, self.dur).with_position(self.pos).resized(width=500)
         print(self.pos)
 
